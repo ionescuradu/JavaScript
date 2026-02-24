dfc3abedd006ebecb5a60982757732d42cba9183
GLOBAL\rionescu <rionescu@sdl.com>
2026-02-24 18:33:21 +0200

    added hashing to the user password

--- Final Application API/src/services/user.service.ts
+++ Final Application API/src/services/user.service.ts
@@ -1,11 +1,14 @@
 import { supabase } from "../lib/supabase";
+import { hashPassword } from "../lib/hash";
 import type { CreateUserDTO, UserDTO } from "../types/user.dto";
 
 export class UserService {
     async createUser(user: CreateUserDTO) {
+        const hashedPassword = await hashPassword(user.password);
+
         const { data, error } = await supabase
             .from("users")
-            .insert(user)
+            .insert({ ...user, password: hashedPassword })
             .select()
             .single()
 
